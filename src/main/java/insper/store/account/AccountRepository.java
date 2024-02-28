fc806d5d345112d3d44bf1ecfa3a632e25c658bf
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-02-28 08:14:29 -0300

    feat: update

---
+++ src/main/java/insper/store/account/AccountRepository.java
@@ -0,0 +1,12 @@
+package insper.store.account;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface AccountRepository  extends CrudRepository<AccountModel, String> {
+
+    Optional<AccountModel> findByEmailAndHash(String email, String hash);
+
+    
+}
